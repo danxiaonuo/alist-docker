584b9a57a4e600ce07a7cb688fed7bf9a7d5242b
danxiaonuo <danxiaonuo@danxiaonuo.me>
2022-09-05 10:49:27 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 # 创建配置文件目录
-mkdir -p /opt/alist/data
+mkdir -p /opt/alist/data/temp /etc/alist/config.json
 
 # 创建配置文件
-cat >/opt/alist/data/config.json <<EOF
+cat >/etc/alist/config.json <<EOF
 {
-    "address":"0.0.0.0",
-    "port":"$PORT",
-    "assets":"$IASSETS",
-    "database":{
-        "type":"$ADATABASE",
-        "user":"$BSQLUSER",
-        "password":"$CSQLPASSWORD",
-        "host":"$DSQLHOST",
-        "port":"$ESQLPORT",
-        "name":"$FSQLNAME",
-        "table_prefix":"x_",
-        "db_file":"data.db"
-    },
-    "scheme":{
-        "https":"false",
-        "cert_file":"",
-        "key_file":""
-    },
-    "cache":{
-        "expiration":"$GEXPIRATION",
-        "cleanup_interval":"$HCLEANUP_INTERVAL"
-    },
-    "temp_dir":"data/temp"
+  "address": "0.0.0.0",
+  "port": $PORT,
+  "assets": "$IASSETS",
+  "database": {
+    "type": "$ADATABASE",
+    "user": "$BSQLUSER",
+    "password": "$CSQLPASSWORD",
+    "host": "$DSQLHOST",
+    "port": $ESQLPORT,
+    "name": "$FSQLNAME",
+    "table_prefix": "x_",
+    "db_file": "/opt/alist/data/data.db"
+  },
+  "scheme": {
+    "https": false,
+    "cert_file": "",
+    "key_file": ""
+  },
+  "cache": {
+    "expiration": $GEXPIRATION,
+    "cleanup_interval": $HCLEANUP_INTERVAL
+  },
+  "temp_dir": "/opt/alist/data/temp"
 }
 EOF
 
