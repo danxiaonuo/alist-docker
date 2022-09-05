9f082a655c785bbadcec743e31a6d91df73e4f35
danxiaonuo <danxiaonuo@danxiaonuo.me>
2022-09-05 09:54:21 +0800

    Update and rename configure.sh.bak to configure.sh

---
+++ configure.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# 创建配置文件目录
+mkdir -p /opt/alist/data/temp /etc/alist/
+
+# 创建配置文件
+cat >/etc/alist/config.json <<EOF
+{
+    "address":"0.0.0.0",
+    "port":"$PORT",
+    "assets":"$IASSETS",
+    "database":{
+        "type":"$ADATABASE",
+        "user":"$BSQLUSER",
+        "password":"$CSQLPASSWORD",
+        "host":"$DSQLHOST",
+        "port":"$ESQLPORT",
+        "name":"$FSQLNAME",
+        "table_prefix":"x_",
+        "db_file":"data.db"
+    },
+    "scheme":{
+        "https":"false",
+        "cert_file":"",
+        "key_file":""
+    },
+    "cache":{
+        "expiration":"$GEXPIRATION",
+        "cleanup_interval":"$HCLEANUP_INTERVAL"
+    },
+    "temp_dir":"data/temp"
+}
+EOF
+# 授权目录权限
+chmod -R 775 /usr/bin/alist /opt/alist
